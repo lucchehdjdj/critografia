71ad5b9864820c0b1bb429b01e4fcdcedc0ea375
lucchehdjdj <79933289+lucchehdjdj@users.noreply.github.com>
2021-03-02 16:57:01 -0300

    Create lucca.py

---
+++ lucca.py
@@ -0,0 +1,23 @@
+
+#!usr/bin/python
+#SCRIPT SIMPLES EM PYTHON!
+#CREDITS: EU MESMO KKKKKKKKKKKKKKKKKKKK: LUCCA UCHIHA
+#APROVEITE!
+import os
+import sys
+red = "\033[91;1m"
+green = "\033[92;1m"
+#fim
+def esolha():
+     print ("red + isso e apenas um teste")
+n1  = int(input(red+'DIGITE 2: '))
+print ('''[ 1 ] lazaro puta do caralho
+[ 2 ] corno pra porra
+''')
+escolha = int(input('Qual é a sua opcao, pra esse corno:'))
+if escolha == 1:
+    print ('kkkkkk, seu merda')
+if escolha == 2:
+    print ("voce q escolheu...corno!")
+else:
+    print ("sabia que voce ia zuar")
