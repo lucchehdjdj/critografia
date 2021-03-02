4d6eddae08192e95342b455310777672235e94c4
lucchehdjdj <79933289+lucchehdjdj@users.noreply.github.com>
2021-03-02 15:00:54 -0300

    Create cripto.sh

---
+++ cripto.sh
@@ -0,0 +1,13 @@
+!/usr/bin/env bash
+#lucca
+
+#codigo fonte 97586f977eb7551587f6df449f96d5e693188ca13e31ad94
+
+
+#CODIFICAR
+read -p "DIGITE SEU TEXTO PARA CODIFICAR: " entrada; entrada="${entrada,,}"
+echo "$entrada" | tr '[a-z]' '[x-za-w]'
+
+#DECODIFICAR:
+read -p "DIGITE SEU TEXTO PARA DECODIFICAR: " entrada
+echo "$entrada" | tr '[x-za-w]' '[a-z]'
